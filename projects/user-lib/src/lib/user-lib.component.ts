b9f810d896172bda4fcf985191bc39067489304c
sunil.gorasiya <sunil.gorasiya@DSK-1137>
2023-08-23 10:52:23 +0530

    inital commit

---
+++ projects/user-lib/src/lib/user-lib.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'user-form',
+  template: `
+    <p>
+      user-lib works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class UserLibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/user-lib/src/lib/user-lib.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { UserLibComponent } from './user-lib.component';
+
+
+
+@NgModule({
+  declarations: [
+    UserLibComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    UserLibComponent
+  ]
+})
+export class UserLibModule { }
